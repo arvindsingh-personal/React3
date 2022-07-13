966d56328deffa47d155d5a8a39a812c946f82dc
arvindsingh-cedcommerce <arvindsingh@cedcoss.com>
2022-07-13 11:03:38 +0530

    Q3. Set the theme as dark, light and party on button click

--- src/Button.js
+++ src/Button.js
@@ -4,7 +4,7 @@ export default class Button extends Component {
   render() {
     if(this.props.color === 'Dark') {
       return (
-        <div className='dark' style={{backgroundColor:'black',height:'100vh',width:'100vw'}}>rfdbhgbvzgvdfgdfsgdsg</div>
+        <div className='dark' style={{backgroundColor:'gray',height:'100vh',width:'100vw'}}>rfdbhgbvzgvdfgdfsgdsg</div>
       )
     }
   }
--- src/Theme.js
+++ src/Theme.js
@@ -1,41 +1,92 @@
 import logo from './logo.svg';
 import './style.css'
-import Button from './Button';
-import  React, {Component}  from 'react';
-
+import React, { Component } from 'react';
 
 
 export default class Theme extends Component {
-  state ={
-    text:'',
-    color:''
+  state = {
+    text: 'Dark',
+    color: '',
+    txt: "White"
   }
-  Change = (event) => {
-    let text = event.target.innerText;
+
+  Change1 = () => {
+
     this.setState({
-      text: text
+      txt: "Dark"
     })
-    
-    
- }
-  
+
+
+  }
+  Change2 = () => {
+
+    this.setState({
+      txt: "Light"
+    })
+
+
+  }
+  Change3 = () => {
+
+    this.setState({
+      txt: "Party"
+    })
+
+
+  }
+
   render() {
-    return (
-      <>
-      <div className="Theme" id='Theme' >
+
+    var result = ""
+
+    if (this.state.txt === "White") {
+
+      result = <div className="Theme" id='Theme' >
         <h1>React Themes Demo</h1>
         <div className='btn'>
-        {/* <Button color={'Dark'} change={this.Change } />
-        <Button color={'Light'} change={this.Change }/>
-        <Button color={'Party'} change={ this.Change }/> */}
-        <button onClick={this.Change}>Dark</button>
-        <button  onClick={this.Change}>Light</button>
-        <button  onClick={this.Change}>Party</button>
-        <Button color={this.state.text}/>
+
         </div>
         <img src={logo} className="App-logo" alt="logo" />
       </div>
+
+    }
+    if (this.state.txt === "Dark") {
+
+      result = <div className="Theme" id='Theme' style={{ backgroundColor: "black" }} >
+        <h1>React Themes Demo</h1>
+        
+        <img src={logo} className="App-logo" alt="logo" />
+      </div>
+
+    }
+    if (this.state.txt === "Light") {
+
+      result = <div className="Theme" id='Theme' style={{ backgroundColor: "lightblue" }}>
+        <h1>React Themes Demo</h1>
+      
+        <img src={logo} className="App-logo" alt="logo" />
+      </div>
+
+    }
+    if (this.state.txt === "Party") {
+
+      result = <div className="Theme" id='Theme' style={{ backgroundImage:'url(https://media0.giphy.com/media/vuNV05pQlsY4psu9IY/giphy.gif)' }}>
+        <h1>React Themes Demo</h1>
+        
+        <img src={logo} className="App-logo" alt="logo" />
+      </div>
+
+    }
+
+    return (
+      <>
+      <div className='btn'>
+        <button onClick={this.Change1}>Dark</button>
+        <button onClick={this.Change2}>Light</button>
+        <button onClick={this.Change3}>Party</button>
+        </div>
+        {result}
       </>
     )
   }
-  }
+}
