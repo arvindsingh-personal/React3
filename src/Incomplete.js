aade2b7bfd3ca14f06335c76884132429986f4bb
arvindsingh-cedcommerce <104122345+arvindsingh-cedcommerce@users.noreply.github.com>
2022-07-20 10:33:34 +0530

    Update Incomplete.js

--- src/Incomplete.js
+++ src/Incomplete.js
@@ -10,7 +10,6 @@ export default class Incomplete extends Component {
           <label>{this.props.task}</label>
           <button className='edit' id={this.props.index} onClick={this.props.Edit}>Edit</button>
           <button className='delete' id={this.props.index} onClick={this.props.Delete} >Delete</button>
-          <p>{this.props.index}</p>
         </li>
       </>
     )
